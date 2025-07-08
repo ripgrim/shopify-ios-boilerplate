8b802ab582a42a513848d4ef59c373e55d5fc26c
grim <75869731+ripgrim@users.noreply.github.com>
2025-07-08 05:42:42 -0700

    liquid ass

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,22 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import {
+  createNativeBottomTabNavigator,
+  NativeBottomTabNavigationEventMap,
+  NativeBottomTabNavigationOptions,
+} from '@bottom-tabs/react-navigation';
+import { ParamListBase, TabNavigationState } from '@react-navigation/native';
+import { withLayoutContext } from 'expo-router';
+
+const BottomTabNavigator = createNativeBottomTabNavigator().Navigator;
+
+const Tabs = withLayoutContext<
+  NativeBottomTabNavigationOptions,
+  typeof BottomTabNavigator,
+  TabNavigationState<ParamListBase>,
+  NativeBottomTabNavigationEventMap
+>(BottomTabNavigator);
 
 export default function TabLayout() {
   const { effectiveColorScheme } = useColorScheme();
@@ -14,58 +24,64 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[effectiveColorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
+        // tabBarActiveTintColor: Colors[effectiveColorScheme ?? 'light'].tint,
+
+        // tabBarButton: HapticTab,
+        // tabBarBackground: TabBarBackground,
+        // tabBarStyle: Platform.select({
+        //   ios: {
+        //     // Use a transparent background on iOS to show the blur effect
+        //     position: 'absolute',
+        //   },
+        //   default: {},
+        // }),
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: () => ({ 
+            sfSymbol: "house",
+            symbolWeight: "thin"
+          }),
         }}
       />
       <Tabs.Screen
         name="ProductsScreen"
         options={{
           title: 'Products',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="square.grid.2x2.fill" color={color} />,
+          tabBarIcon: () => ({ 
+            sfSymbol: "square.grid.2x2",
+            symbolWeight: "medium"
+          }),
         }}
       />
       <Tabs.Screen
         name="CollectionsScreen"
         options={{
           title: 'Collections',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="rectangle.stack.fill" color={color} />,
+          tabBarIcon: () => ({ sfSymbol: "rectangle.stack" }),
         }}
       />
       <Tabs.Screen
         name="AccountScreen"
         options={{
           title: 'Account',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
+          tabBarIcon: () => ({ sfSymbol: "person" }),
         }}
       />
       <Tabs.Screen
         name="SettingsScreen"
         options={{
           title: 'Settings',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="gearshape.fill" color={color} />,
+          tabBarIcon: () => ({ sfSymbol: "gearshape" }),
         }}
       />
       <Tabs.Screen
         name="TestScreen"
         options={{
           title: 'Test',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="checkmark.circle.fill" color={color} />,
+          tabBarIcon: () => ({ sfSymbol: "checkmark.circle" }),
         }}
       />
     </Tabs>
